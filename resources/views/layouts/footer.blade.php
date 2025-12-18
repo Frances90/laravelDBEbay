c36a6f9e67deb57411bfbddbdf9a8bf76e7efc31
Frances <francesgafert@hotmail.com>
2025-12-18 12:32:36 +0100

    passendes Design für den Header sowie den Footer Bereich angelegt

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,38 @@
 <footer>
-    <div>
-        <p>&copy; {{ date('Y') }} DBEbay. Alle Rechte vorbehalten.</p>
-        <nav>
+    <div id="container_verwalten">
+        <img src="{{ asset('img/logo.svg') }}" alt="Logo der App">
+        <div class="container_layout" id="container_unternehmen">
+            <h2>Unternehmen</h2>
+            <a href="#">Über uns</a>
+            <a href="#">Karriere</a>
+            <a href="#">Newsletter</a>
+            <a href="#">Hilfebereich</a>
+        </div>
+        <div class="container_layout" id="container_rechtliches">
+            <h2>Rechtliches</h2>
             <a href="#">Impressum</a>
             <a href="#">Datenschutz</a>
             <a href="#">AGB</a>
-        </nav>
-        <div class="social-media">
-            <a href="#">Instagram</a>
-            <a href="#">Facebook</a>
-            <a href="#">YouTube</a>
-            <a href="#">TikTok</a>
+        </div>
+        <div>
+            <div class="social_media">
+                <h2>Social Media</h2>
+                <div id="icons_sm">
+                <a class="link_sm" href="#">
+                    <img class="icon" src="{{ asset('img/instagram.svg') }}" alt="Instagram Icon">
+                </a>
+                <a class="link_sm" href="#">
+                    <img class="icon" src="{{ asset('img/facebook.svg') }}" alt="Facebook Icon">
+                </a>
+                <a class="link_sm" href="#">
+                    <img class="icon" src="{{ asset('img/youtube.svg') }}" alt="YouTube Icon">
+                </a>
+                <a class="link_sm" href="#">
+                    <img class="icon" src="{{ asset('img/tiktok.svg') }}" alt="TikTok Icon">
+                </a>
+                </div>
+            </div>
+            <p>&copy; {{ date('Y') }} DBEbay. Alle Rechte vorbehalten.</p>
         </div>
     </div>
 </footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,40 @@
 <header>
-    <nav>
+    <nav id="nav_header">
+        <a href="{{ route('listings.index') }}">
+            <img src="{{ asset('img/logo.svg') }}" alt="Logo der App">
+        </a>
+        
+        <div class="container_search">
+            <div class="group_inputs" id="input_lupe">
+                <img class="icon" src="{{ asset('img/lupe.svg') }}" alt="Lupe für die Suche">
+                <input type="text" placeholder="Was suchst du?" id="input_search" />
+            </div>
+
+            <div class="group_inputs">
+                <img class="icon" src="{{ asset('img/location.svg') }}" alt="Lupe für die Suche">
+                <input type="text" placeholder="PLZ oder Ort" />
+            </div>
+
+            <button class="btn_search">Suchen</button>
+        </div>
+
         <ul>
-            <li><a href="{{ route('listings.index') }}">Startseite</a></li>
-            <li><a href="{{ route('listings.create') }}">Neues Listing</a></li>
-            <li><a href="#">Kategorien</a></li>
-            <li><a href="#">Mein Konto</a></li>
+            <li>
+                <a href="#">
+                    <img src="{{ asset('img/profile.svg') }}" alt="Konto des Benutzers">
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('listings.create') }}">
+                    <img src="{{ asset('img/create_listing.svg') }}" alt="Neue Artikel anlegen">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="{{ asset('img/heart.svg') }}" alt="Favoriten">
+                </a>
+            </li>
+            
         </ul>
     </nav>
 </header>
