ce728b7218bc42d1aaf31a0b941595e13e647fa2
Frances <francesgafert@hotmail.com>
2026-01-07 12:27:35 +0100

    E-Mails versenden nach dem ein User sich registriert hat

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use App\Mail\WelcomeMail;
+use Illuminate\Support\Facades\Mail;
 
 class RegisteredUserController extends Controller
 {
@@ -55,6 +57,7 @@ class RegisteredUserController extends Controller
 
         Auth::login($customer);
 
+        Mail::to($customer->email)->send(new WelcomeMail($customer));
         return redirect(route('Startseite', absolute: false));
     }
 }
